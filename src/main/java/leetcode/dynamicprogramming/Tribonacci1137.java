a57dd46357cbd34e996f9a391293b6c9e365aca2
JeremiahJ <9273585+Jeremiah1@users.noreply.github.com>
2021-12-31 12:13:07 -0600

    LeetCode: Solution and Tests for problem 1137

---
+++ src/main/java/leetcode/dynamicprogramming/Tribonacci1137.java
@@ -0,0 +1,41 @@
+package leetcode.dynamicprogramming;
+
+// https://leetcode.com/problems/n-th-tribonacci-number/
+// Problem 1137: N-th Tribonacci Number
+public class Tribonacci1137 {
+
+    private Tribonacci1137(){}
+
+    public static Integer tribonacci(int n) {
+
+        int[] tribNums = new int[Math.max(3, n+1)];
+        tribNums[0] = 0;
+        tribNums[1] = tribNums[2] = 1;
+
+        for(int i = 3; i < n+1; i++){
+            tribNums[i] = tribNums[i-1] + tribNums[i-2] + tribNums[i-3];
+        }
+        
+        return tribNums[n];
+    }
+
+    public static Integer tribonacciMemoized(int n) {
+        
+        int[] tribNums = new int[Math.max(3, n+1)];
+
+        return tribonacciRecursion(tribNums, n);
+    }
+
+    private static Integer tribonacciRecursion(int[] tribNums, int tribIndex){
+
+        if(tribIndex == 0) return 0;
+        if(tribIndex == 1 || tribIndex == 2) return 1;
+
+        int a = (tribNums[tribIndex-1] > 0) ? tribNums[tribIndex-1] : tribonacciRecursion(tribNums, tribIndex-1);
+        int b = (tribNums[tribIndex-2] > 0) ? tribNums[tribIndex-2] : tribonacciRecursion(tribNums, tribIndex-2);
+        int c = (tribNums[tribIndex-3] > 0) ? tribNums[tribIndex-3] : tribonacciRecursion(tribNums, tribIndex-3);
+        
+        tribNums[tribIndex] = a + b + c;
+        return a + b + c;
+    }
+}
---
+++ src/test/java/leetcode/dynamicprogramming/Tribonacci1137Test.java
@@ -0,0 +1,76 @@
+package leetcode.dynamicprogramming;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class Tribonacci1137Test {
+
+    @Test
+    void givenWholeNumberN_thenReturnNthTribonacciNumber() {
+
+        int given = 4;
+        int expected = 4;
+        int result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isEqualTo(expected);
+
+        given = 10;
+        expected = 149;
+        result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isEqualTo(expected);
+
+        given = 25;
+        expected = 1389537;
+        result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void givenWholeNumberN_thenReturnNthTribonacciWithMemoizedRecursion() {
+
+        int given = 4;
+        int expected = 4;
+        int result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isEqualTo(expected);
+
+        given = 10;
+        expected = 149;
+        result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isEqualTo(expected);
+
+        given = 25;
+        expected = 1389537;
+        result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void givenZero_thenReturnZero() {
+
+        int given = 0;
+        int result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isZero();
+
+        result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isZero();
+    }
+
+    @Test
+    void givenOneOrTwo_thenReturnOne() {
+
+        int given = 1;
+        int expected = 1;
+        int result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isEqualTo(expected);
+
+        result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isEqualTo(expected);
+
+        given = 2;
+        result = Tribonacci1137.tribonacci(given);
+        assertThat(result).isEqualTo(expected);
+
+        result = Tribonacci1137.tribonacciMemoized(given);
+        assertThat(result).isEqualTo(expected);
+    }
+}
