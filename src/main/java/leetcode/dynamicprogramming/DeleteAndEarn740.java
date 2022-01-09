089f47b7931361d77c191ed336119632c2749f7b
JeremiahJ <9273585+Jeremiah1@users.noreply.github.com>
2022-01-08 21:45:56 -0600

    LeetCode: Solution and Tests for problem 740

---
+++ src/main/java/leetcode/dynamicprogramming/DeleteAndEarn740.java
@@ -0,0 +1,32 @@
+package leetcode.dynamicprogramming;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// https://leetcode.com/problems/delete-and-earn/
+// Problem 740: Delete and Earn
+public class DeleteAndEarn740 {
+
+    private DeleteAndEarn740(){}
+
+    public static Integer deleteAndEarn(int[] nums) {
+
+        int[] count = new int[10_001];
+        for(int x : nums) count[x]++;
+        
+        int[] sum = new int[10_001];
+        sum[1] = count[1];
+        sum[2] = Math.max(count[1], 2*count[2]);
+        
+        for(int i = 3; i < 10_001; i++){
+            sum[i] = Math.max(sum[i-1], sum[i-2] + i*count[i]);
+        }
+        return sum[10_000];
+    }
+}
---
+++ src/test/java/leetcode/dynamicprogramming/DeleteAndEarn740Test.java
@@ -0,0 +1,31 @@
+package leetcode.dynamicprogramming;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DeleteAndEarn740Test {
+
+    @Test
+    void givenListOfNumbers_thenReturnMaximumPoints() {
+
+        int[] given = {3, 4, 2};
+        int expected = 6;
+        int result = DeleteAndEarn740.deleteAndEarn(given);
+        Assertions.assertThat(result).isEqualTo(expected);
+
+        given = new int[]{2, 2, 3, 3, 3, 4};
+        expected = 9;
+        result = DeleteAndEarn740.deleteAndEarn(given);
+        Assertions.assertThat(result).isEqualTo(expected);
+
+        given = new int[]{2, 3};
+        expected = 3;
+        result = DeleteAndEarn740.deleteAndEarn(given);
+        Assertions.assertThat(result).isEqualTo(expected);
+
+        given = new int[]{10};
+        expected = 10;
+        result = DeleteAndEarn740.deleteAndEarn(given);
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
